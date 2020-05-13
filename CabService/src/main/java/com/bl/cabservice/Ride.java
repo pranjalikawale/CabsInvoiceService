2b0dba4d9a81529f09b0afdb7301af93fd27956f
pranjalikawale <pranjalikawale1578@gmail.com>
2020-05-13 16:21:32 +0530

    ADDED: Calculate fare multiple ride

---
+++ CabService/src/main/java/com/bl/cabservice/Ride.java
@@ -0,0 +1,11 @@
+package com.bl.cabservice;
+
+public class Ride {
+    public double distance;
+    public final int time;
+
+    public Ride(double distance, int time) {
+        this.distance=distance;
+        this.time=time;
+    }
+}
--- CabService/src/test/java/com/bl/cabservice/InvoiceServiceTest.java
+++ CabService/src/test/java/com/bl/cabservice/InvoiceServiceTest.java
@@ -20,6 +20,7 @@ public class InvoiceServiceTest {
         Assert.assertEquals(25,fare,0.0);
     }
 
+
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinFare(){
         double distance=0.1;
@@ -27,4 +28,15 @@ public class InvoiceServiceTest {
         double fare=invoiceGenerator.calculateFare(distance,time);
         Assert.assertEquals(5,fare,0.0);
     }
+
+    @Test
+    public void givenMultipleRide_ShouldReturnTotalFare(){
+        Ride[] rides={new Ride(2.0,5),
+                      new Ride(0.1,1)};
+        double fare =invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30,fare,0.0);
+    }
+
+
+
 }
