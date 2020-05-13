9b84b2d465f75d7164aaa2ba2878f58b6a822f4e
pranjalikawale <pranjalikawale1578@gmail.com>
2020-05-13 15:51:13 +0530

    ADDED: For less distance time return minimum fare

--- CabService/src/main/java/com/bl/cabservice/InvoiceGenerator.java
+++ CabService/src/main/java/com/bl/cabservice/InvoiceGenerator.java
@@ -3,8 +3,13 @@ package com.bl.cabservice;
 public class InvoiceGenerator {
     private static final int COST_PR_TIME = 1;
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final double MINIMUM_FARE=5;
 
     public double calculateFare(double distance, int time) {
-        return distance*MINIMUM_COST_PER_KILOMETER+time*COST_PR_TIME;
+        double totalFare=distance*MINIMUM_COST_PER_KILOMETER+time*COST_PR_TIME;
+        if(totalFare<MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
+
     }
 }
--- CabService/src/test/java/com/bl/cabservice/InvoiceServiceTest.java
+++ CabService/src/test/java/com/bl/cabservice/InvoiceServiceTest.java
@@ -19,4 +19,12 @@ public class InvoiceServiceTest {
         double fare=invoiceGenerator.calculateFare(distance,time);
         Assert.assertEquals(25,fare,0.0);
     }
+
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare(){
+        double distance=0.1;
+        int time=1;
+        double fare=invoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals(5,fare,0.0);
+    }
 }
