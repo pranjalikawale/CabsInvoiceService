5d8674201c2c1d1c2a2b352dfcdab4ca6c30c2ce
pranjalikawale <pranjalikawale1578@gmail.com>
2020-05-14 14:59:34 +0530

    ADDED: Return invoice for list of rides

--- CabService/src/main/java/com/bl/cabservice/InvoiceService.java
+++ CabService/src/main/java/com/bl/cabservice/InvoiceService.java
@@ -1,16 +1,17 @@
 package com.bl.cabservice;
 
-public class InvoiceGenerator {
+public class InvoiceService {
     private static final int COST_PR_TIME = 1;
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
     private static final double MINIMUM_FARE=5;
+    private final RideRepository rideRepository;
 
+    public InvoiceService(){
+        this.rideRepository=new RideRepository();
+    }
     public double calculateFare(double distance, int time) {
         double totalFare=distance*MINIMUM_COST_PER_KILOMETER+time*COST_PR_TIME;
-        if(totalFare<MINIMUM_FARE)
-            return MINIMUM_FARE;
-        return totalFare;
-
+        return Math.max(totalFare,MINIMUM_FARE);
     }
 
     public InvoiceSummary  calculateFare(Ride[] rides) {
@@ -19,5 +20,13 @@ public class InvoiceGenerator {
             totalFare+=this.calculateFare(ride.distance,ride.time);
         }
         return new InvoiceSummary(rides.length,totalFare);
+    }
+
+    public void addRides(String userId,Ride[] rides){
+        rideRepository.addRides(userId,rides);
+    }
+
+    public InvoiceSummary getInvoiceSummary(String userId){
+        return this.calculateFare(rideRepository.getRides(userId));
     }
 }
--- CabService/src/test/java/com/bl/cabservice/InvoiceServiceTest.java
+++ CabService/src/test/java/com/bl/cabservice/InvoiceServiceTest.java
@@ -5,18 +5,18 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
-    InvoiceGenerator invoiceGenerator=null;
+    InvoiceService invoiceService =null;
 
     @Before
     public void initialize(){
-        invoiceGenerator=new InvoiceGenerator();
+        invoiceService =new InvoiceService();
     }
 
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare(){
         double distance=2.0;
         int time=5;
-        double fare=invoiceGenerator.calculateFare(distance,time);
+        double fare= invoiceService.calculateFare(distance,time);
         Assert.assertEquals(25,fare,0.0);
     }
 
@@ -25,7 +25,7 @@ public class InvoiceServiceTest {
     public void givenLessDistanceAndTime_ShouldReturnMinFare(){
         double distance=0.1;
         int time=1;
-        double fare=invoiceGenerator.calculateFare(distance,time);
+        double fare= invoiceService.calculateFare(distance,time);
         Assert.assertEquals(5,fare,0.0);
     }
 
@@ -33,11 +33,22 @@ public class InvoiceServiceTest {
     public void givenMultipleRide_ShouldReturnTotalFare(){
         Ride[] rides={new Ride(2.0,5),
                       new Ride(0.1,1)};
-        InvoiceSummary summary=invoiceGenerator.calculateFare(rides);
+        InvoiceSummary summary= invoiceService.calculateFare(rides);
         InvoiceSummary expectedInvoiceSummary=new InvoiceSummary(2,30.0);
         Assert.assertEquals(expectedInvoiceSummary,summary);
     }
 
-
-
+    @Test
+    public void givenUserIdAndRides_ShouldReturnInvoiceSummary(){
+        String userId="pranjali@123.com";
+        Ride[] rides={new Ride(2.0,5),
+                new Ride(0.1,1)};
+        invoiceService.addRides(userId,rides);
+        Ride[] rides1={new Ride(2.0,5),
+                new Ride(0.1,1)};
+        invoiceService.addRides(userId,rides1);
+        InvoiceSummary summary= invoiceService.getInvoiceSummary(userId);
+        InvoiceSummary expectedInvoiceSummary=new InvoiceSummary(2,30.0);
+        Assert.assertEquals(expectedInvoiceSummary,summary);
+    }
 }
